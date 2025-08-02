dfa6367f9707d89d032a246e147d823f47012a8c
Cowfweee <76927388+Cowfweee@users.noreply.github.com>
2025-08-01 21:38:45 -0400

    Create NpcAiMod.java

---
+++ src/main/java/com/cowfweee/minecraftai/NpcAiMod.java
@@ -0,0 +1,12 @@
+package com.cowfweee.minecraftai;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NpcAiMod implements ModInitializer {
+    public static final String MOD_ID = "minecraftai";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("NPC AI Mod initialized!");
+    }
+}
